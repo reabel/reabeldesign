0f36cf504ae23019bb14bab7bd95968fd54260fd
reabel <ryanreabel@gmail.com>
2024-01-11 19:38:22 -0500

    testing routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import Links from "./components/Links";
 
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
 function App() {
+  const Home = () => (<span>Home</span>);
+  const Dashboard = () => (<span>Dashboard</span>);
+  const Guides = () => (<span>Guides</span>);
   return (
+    
     <div className="App">
-      <body>
         <div styleClass="main-body">
           <h1>ReabelDesign</h1>
           <Links />
+          <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/guides" element={<Guides />} />
+            </Routes>
+          </BrowserRouter>
         </div>
-      </body>
       <footer>
         © <a href="http://reabelx.com">ReabelX</a> 2024
       </footer>
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { NavLink } from 'react-router-dom';
 class Links extends Component {
   render() {
     return (
