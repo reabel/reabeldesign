7cd7327acea435b5efa914ee8bf3ed10939a7a56
reabel <ryanreabel@gmail.com>
2024-01-16 14:36:08 -0500

    Working on building out initial experiments

--- src/components/Experiments.tsx
+++ src/components/Experiments.tsx
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
+import './experiments.css';
+type Experiment = {
+  name: String;
+  value: String;
+  className: any;
+}
+
+
+const fonts: Array<Experiment> = [
+  { name: 'Shadow', value: 'Shadow', className: 'shadow'},
+  { name: 'Shadow 2', value: 'Shadow 2', className: 'shadow2'},
+]
+
+const listFonts = fonts.map((font: Experiment, index) => {
+  return (
+    <div key={ font.name + index.toString() } className="experi-container">
+      <div className="experi-box">
+        <div className={font.className}>
+          {font.value}
+        </div>
+      </div>
+      <div className="experi-nfo">
+          {font.name}: {font.className}
+      </div>
+    </div>
+  );
+    
+})
+
 class Experiments extends Component {
   render() {
     return (
-      <div id="experiments">
-        <div className="experi-box">
-            Test
-        </div>
-        <div className="experi-nfo">
-            Test
-        </div>
+      <div>
+        {listFonts}
       </div>
     );
   }
