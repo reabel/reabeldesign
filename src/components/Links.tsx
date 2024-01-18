0b4f9feb5d292d759c8c228a148e28d9350232c3
reabel <ryanreabel@gmail.com>
2024-01-18 16:14:54 -0500

    Rebuilding UI in Chakra

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,26 +1,44 @@
-import React, { Component } from "react";
-class Links extends Component {
-  render() {
-    return (
-        <>
-          <a href="http://blog.reabelx.com" target="blank">
-            Blog
-          </a>
-          <a href="https://www.instagram.com/reabel/" target="blank">
-            Photography
-          </a>
-          <a href="https://twitter.com/reabel" target="blank">
-            Social
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ryan-reabel-a7a33712/"
-            target="blank"
-          >
-            LinkedIn
-          </a>
-        </>
-    );
-  }
+import { Button, ButtonGroup, LinkOverlay } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom';
+const Links = () => {
+  
+  const navigate = useNavigate();
+  
+  return (
+    <ButtonGroup isAttached>
+      {/* <nav class="nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/experiments">Experiments</NavLink>
+      </nav>  */}
+      <Button onClick={() => {navigate('/')}} colorScheme='whatsapp' variant='solid'>
+        Home
+      </Button>
+      <Button onClick={() => {navigate('/experiments')}} colorScheme='whatsapp' variant='solid'>
+        Experiments
+      </Button>
+      <Button  colorScheme='whatsapp' variant='solid'>
+        <LinkOverlay href='http://blog.reabelx.com' target="blank">
+          Blog
+        </LinkOverlay>
+      </Button>
+      <Button colorScheme='whatsapp'>
+        <LinkOverlay href="https://www.instagram.com/reabel/" target="blank">
+          Photography
+        </LinkOverlay>
+      </Button>
+      <Button colorScheme='whatsapp'>
+        <LinkOverlay href="https://twitter.com/reabel" target="blank">
+        Social
+        </LinkOverlay>
+      </Button>
+      <Button colorScheme='whatsapp'>
+        <LinkOverlay href="https://www.linkedin.com/in/ryan-reabel-a7a33712/"
+        target="blank">
+          LinkedIn
+        </LinkOverlay>
+      </Button>
+    </ButtonGroup>
+  );
 }
 
 export default Links;
