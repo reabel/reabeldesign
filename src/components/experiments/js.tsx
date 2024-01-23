8cddbf68b81769e040a3e2bd111c75dae5b12fa2
reabel <ryanreabel@gmail.com>
2024-01-23 15:20:21 -0500

    adding initial JS experiments.

--- src/components/experiments/js.tsx
+++ src/components/experiments/js.tsx
@@ -1,11 +1,43 @@
 import { Component } from "react";
-import { TabPanel } from '@chakra-ui/react'
+import { Card, CardBody, Code, TabPanel } from '@chakra-ui/react';
+
+
+type Experiment = {
+    name: String;
+    value: String;
+    className: any;
+    style: String;
+  }
+  
+  const font: Experiment ={ name: 'Shadow', value: 'Shadow', className: 'shadow', style: 'filter: drop-shadow(0 0 0.75rem crimson);'};
 
 class ExperimentsJS extends Component {
     render () {
+        // Not working currently, cheating the html portion
+        // May be better to see if there's a prebuilt component to let me do these, and just populate with the code.
+        const testHTML = (
+            <div>
+                <p id="test">Test2</p>
+                <script>
+                    document.getElementById("test").innerHTML = "Test2";
+                </script>
+            </div>
+        );
+
         return (
             <TabPanel>
-                <p>JS</p>
+                <div key={ font.name + '1' } className="experi-container">
+                    <Card align="center" size="lg" style={{margin: "10px"}}>
+                    <CardBody>
+                        {testHTML}
+                    </CardBody>
+                    </Card>
+                    <Card size="lg" variant="filled" style={{margin: "10px"}}>
+                    <CardBody>
+                        <div ><Code colorScheme="whatsapp">document.getElementById("test").innerHTML = "Test2";</Code></div>
+                    </CardBody>
+                    </Card>
+                </div>
             </TabPanel>
         )
     }
