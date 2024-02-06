b3acfa326f3f480b7e24887ae8760490b572c3ee
reabel <ryanreabel@gmail.com>
2024-02-06 14:25:04 -0500

    Fixing links, adding icons.

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,5 +1,7 @@
-import { Button, ButtonGroup, LinkOverlay, Menu, MenuItem, MenuButton, MenuList } from '@chakra-ui/react'
+import { Button, ButtonGroup, LinkOverlay, Link, Menu, MenuItem, MenuButton, MenuList } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
+import { FaLinkedin, FaFlickr, FaInstagram } from 'react-icons/fa'; //https://react-icons.github.io/react-icons/#/
+import { FaXTwitter } from 'react-icons/fa6' 
 import { useNavigate } from 'react-router-dom';
 const Links = () => {
   
@@ -23,26 +25,38 @@ const Links = () => {
         </LinkOverlay>
       </Button>
       {/* TOOD: Instead of linking to instagram, create an internal page w/ a photo grid */}
-      <Button colorScheme='whatsapp'>
-        <LinkOverlay href="https://www.instagram.com/reabel/" target="blank">
+      <Menu>
+        <MenuButton as={Button} colorScheme='whatsapp' rightIcon={<ChevronDownIcon />}>
           Photography
-        </LinkOverlay>
-      </Button>
+        </MenuButton>
+        <MenuList>
+          <MenuItem>
+            <Link href="https://www.instagram.com/reabel/" target="blank" isExternal>
+              <FaInstagram /> Instagram
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="https://www.flickr.com/photos/reabel/" target="blank" isExternal>
+              <FaFlickr /> Flickr
+            </Link>
+          </MenuItem>
+        </MenuList>
+      </Menu>
       <Menu>
         <MenuButton as={Button} colorScheme='whatsapp' rightIcon={<ChevronDownIcon />}>
           Social
         </MenuButton>
         <MenuList>
           <MenuItem>
-            <LinkOverlay href="https://twitter.com/reabel" target="blank">
-            Twitter
-            </LinkOverlay>
+            <Link href="https://twitter.com/reabel" target="blank" isExternal>
+            <FaXTwitter /> Twitter
+            </Link>
           </MenuItem>
           <MenuItem >
-            <LinkOverlay href="https://www.linkedin.com/in/ryan-reabel-a7a33712/"
-            target="blank">
-              LinkedIn
-            </LinkOverlay>
+            <Link href="https://www.linkedin.com/in/ryan-reabel-a7a33712/"
+            target="blank" isExternal>
+              <FaLinkedin /> LinkedIn
+            </Link>
           </MenuItem>
         </MenuList>
       </Menu>
